ea4b8f6cc7353598841dfae4fa263bf600e9adfd
cribeul <christian.perwass@de.bosch.com>
2023-11-23 15:36:37 +0100

    adapts code to blender 4
    Signed-off-by: cribeul <christian.perwass@de.bosch.com>

--- src/anyblend/node/shader/utils.py
+++ src/anyblend/node/shader/utils.py
@@ -32,7 +32,7 @@ import bpy
 from .types import TData
 import numbers
 
-from typing import NamedTuple, Any, Union
+from typing import NamedTuple, Any, Union, Optional
 from dataclasses import dataclass
 from enum import Enum
 
@@ -141,28 +141,112 @@ def _ConnectWithSocket(xSNT: bpy.types.NodeTree, xTrg: bpy.types.NodeSocket, xSr
 
 
 ###################################################################
-def _ProvideNodeTreeInputSocket(ntGrp: bpy.types.NodeTree, sName: str, sTypeName: str, xValue: Any):
-    if ntGrp.inputs.get(sName) is None:
-        ntGrp.inputs.new(sTypeName, sName)
-    # endif
-
-    try:
-        if hasattr(ntGrp.inputs[sName], "default_value"):
-            ntGrp.inputs[sName].default_value = xValue
-        # endif
-    except Exception as xEx:
-        raise CAnyError_Message(sMsg=f"Error setting default value of '{sName}' to '{xValue}'", xChildEx=xEx)
-    # endtry
+def GetNodeTreeInputNode(ntGrp: bpy.types.NodeTree) -> Optional[bpy.types.NodeGroupInput]:
+    return next((x for x in ntGrp.nodes if x.type == "GROUP_INPUT"), None)
 
 
 # enddef
 
 
 ###################################################################
-def _ProvideNodeTreeOutputSocket(ntGrp: bpy.types.NodeTree, sName: str, sTypeName: str):
-    if ntGrp.outputs.get(sName) is None:
-        ntGrp.outputs.new(sTypeName, sName)
+def GetNodeTreeOutputNode(ntGrp: bpy.types.NodeTree) -> Optional[bpy.types.NodeGroupOutput]:
+    return next((x for x in ntGrp.nodes if x.type == "GROUP_OUTPUT"), None)
+
+
+# enddef
+
+
+###################################################################
+def GetNodeTreeInputs(ntGrp: bpy.types.NodeTree, *, _bDoRaise: bool = True) -> Optional[bpy.types.NodeOutputs]:
+    ndX: bpy.types.Node = GetNodeTreeInputNode(ntGrp)
+    if ndX is None:
+        if _bDoRaise is True:
+            raise RuntimeError(f"Node tree '{ntGrp.name}' has not group input node")
+        else:
+            return None
+        # endif
     # endif
+
+    return ndX.outputs
+
+
+# enddef
+
+
+###################################################################
+def GetNodeTreeOutputs(ntGrp: bpy.types.NodeTree, *, _bDoRaise: bool = True) -> Optional[bpy.types.NodeInputs]:
+    ndX: bpy.types.Node = GetNodeTreeOutputNode(ntGrp)
+    if ndX is None:
+        if _bDoRaise is True:
+            raise RuntimeError(f"Node tree '{ntGrp.name}' has not group output node")
+        else:
+            return None
+        # endif
+    # endif
+
+    return ndX.inputs
+
+
+# enddef
+
+
+###################################################################
+def ProvideNodeTreeInputSocket(
+    ntGrp: bpy.types.NodeTree, sName: str, sTypeName: str, xValue: Any
+) -> bpy.types.NodeSocket:
+    # For Blender < 4.0
+    if hasattr(ntGrp, "inputs"):
+        xInput = ntGrp.inputs.get(sName)
+        if xInput is None:
+            xInput = ntGrp.inputs.new(sTypeName, sName)
+        # endif
+
+    # For Blender >= 4.0
+    elif hasattr(ntGrp, "interface"):
+        xInput = next((x for x in ntGrp.interface.items_tree if x.name == sName and x.in_out == "INPUT"), None)
+        if xInput is None:
+            xInput = ntGrp.interface.new_socket(sName, in_out="INPUT", socket_type=sTypeName)
+        # endif
+
+    else:
+        raise RuntimeError("Unsupported node tree object. Maybe new Blender version with breaking changes.")
+    # endif
+
+    try:
+        if hasattr(xInput, "default_value"):
+            xInput.default_value = xValue
+        # endif
+    except Exception as xEx:
+        raise CAnyError_Message(sMsg=f"Error setting default value of '{sName}' to '{xValue}'", xChildEx=xEx)
+    # endtry
+
+    return xInput
+
+
+# enddef
+
+
+###################################################################
+def ProvideNodeTreeOutputSocket(ntGrp: bpy.types.NodeTree, sName: str, sTypeName: str) -> bpy.types.NodeSocket:
+    # For Blender < 4.0
+    if hasattr(ntGrp, "outputs"):
+        xOutput = ntGrp.outputs.get(sName)
+        if xOutput is None:
+            xOutput = ntGrp.outputs.new(sTypeName, sName)
+        # endif
+
+    # For Blender >= 4.0
+    elif hasattr(ntGrp, "interface"):
+        xOutput = next((x for x in ntGrp.interface.items_tree if x.name == sName and x.in_out == "OUTPUT"), None)
+        if xOutput is None:
+            xOutput = ntGrp.interface.new_socket(sName, in_out="OUTPUT", socket_type=sTypeName)
+        # endif
+
+    else:
+        raise RuntimeError("Unsupported node tree object. Maybe new Blender version with breaking changes.")
+    # endif
+
+    return xOutput
 
 
 # enddef
@@ -175,7 +259,7 @@ def ProvideNodeTreeInputs(
 ) -> bpy.types.NodeGroupInput:
     if isinstance(xData, list):
         for lItem in xData:
-            _ProvideNodeTreeInputSocket(ntGrp, lItem[0], lItem[1], lItem[2])
+            ProvideNodeTreeInputSocket(ntGrp, lItem[0], lItem[1], lItem[2])
         # endfor
 
     elif isinstance(xData, CNodeSocketCollection):
@@ -184,18 +268,18 @@ def ProvideNodeTreeInputs(
                 raise CAnyError_Message(sMsg="CNodeSocketCollection object contains element of invalid type")
             # endif
 
-            _ProvideNodeTreeInputSocket(ntGrp, xItem.sName, xItem.typSocket.__name__, xItem.xValue)
+            ProvideNodeTreeInputSocket(ntGrp, xItem.sName, xItem.typSocket.__name__, xItem.xValue)
         # endfor
     else:
         raise CAnyError_Message(sMsg="Invalid input type of element 'xData'")
     # endif
 
-    nodIn = ntGrp.nodes.get("Group Input")
-    if nodIn is None:
-        nodIn = ntGrp.nodes.new("NodeGroupInput")
+    ndIn = GetNodeTreeInputNode(ntGrp)
+    if ndIn is None:
+        ndIn = ntGrp.nodes.new("NodeGroupInput")
     # endif
 
-    return nodIn
+    return ndIn
 
 
 # enddef
@@ -208,7 +292,7 @@ def ProvideNodeTreeOutputs(
 ) -> bpy.types.NodeGroupOutput:
     if isinstance(xData, list):
         for lItem in xData:
-            _ProvideNodeTreeOutputSocket(ntGrp, lItem[0], lItem[1])
+            ProvideNodeTreeOutputSocket(ntGrp, lItem[0], lItem[1])
 
         # endfor
 
@@ -218,18 +302,18 @@ def ProvideNodeTreeOutputs(
                 raise CAnyError_Message(sMsg="CNodeSocketCollection object contains element of invalid type")
             # endif
 
-            _ProvideNodeTreeOutputSocket(ntGrp, xItem.sName, xItem.typSocket.__name__)
+            ProvideNodeTreeOutputSocket(ntGrp, xItem.sName, xItem.typSocket.__name__)
         # endfor
     else:
         raise CAnyError_Message(sMsg="Invalid input type of element 'xData'")
     # endif
 
-    nodOut = ntGrp.nodes.get("Group Output")
-    if nodOut is None:
-        nodOut = ntGrp.nodes.new("NodeGroupOutput")
+    ndOut = GetNodeTreeOutputNode(ntGrp)
+    if ndOut is None:
+        ndOut = ntGrp.nodes.new("NodeGroupOutput")
     # endif
 
-    return nodOut
+    return ndOut
 
 
 # enddef
